baf285e55a20896a4ed894abf014c167b0379105
sierraxtine <sierraxtine@gmail.com>
2025-07-24 22:46:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -117,3 +117,18 @@ function showFinalScreen() {
     window.location.href = mailtoLink;
   };
 }
+<script>
+  function createStar() {
+    const star = document.createElement('div');
+    star.classList.add('star');
+    star.style.left = `${Math.random() * 100}vw`;
+    star.style.animationDuration = `${Math.random() * 3 + 2}s`;
+    document.body.appendChild(star);
+
+    setTimeout(() => {
+      star.remove();
+    }, 5000);
+  }
+
+  setInterval(createStar, 100);
+</script>
